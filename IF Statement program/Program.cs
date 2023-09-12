5b7d9bb16e6f16c1fed4ddc899da0e94c8281c89
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-13 00:28:44 +0200

    Add project files.

---
+++ IF Statement program/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_Statement_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            // NB ! means not, or negation
+            if (isMale)
+            {
+                Console.WriteLine("you are male");
+            }
+            else
+            {
+                Console.WriteLine("you are female");
+            }
+            bool isTall = true;
+            if (isTall && isMale) {
+                Console.WriteLine(" You are both male and tall");
+
+            }
+            else if (isTall && isMale)
+            {
+                Console.WriteLine("you are a short man");
+            }
+            else if(!(isMale && isTall))
+            {
+                Console.WriteLine("you are neither a man tall");
+            }
+            else {
+                Console.WriteLine("You are a woman");
+            }
+
+
+            Console.Read();
+        }
+    }
+}
